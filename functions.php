402b7d6d69eda522d460e19ee4067e3c7338106f
bradyontech <brady.on.tech@outlook.com>
2026-01-27 14:21:25 -0600

    Update author base URL logic for blog path
    Adjusts the author base URL to 'author' if the home path is 'blog', otherwise defaults to 'blog/author'. This ensures correct author URL structure depending on the site's home path.

--- functions.php
+++ functions.php
@@ -644,7 +644,8 @@ add_action('wp_head', 'automatdo_website_schema', 5);
  */
 function automatdo_set_author_base() {
     global $wp_rewrite;
-    $wp_rewrite->author_base = 'blog/author';
+    $home_path = trim((string) wp_parse_url(home_url('/'), PHP_URL_PATH), '/');
+    $wp_rewrite->author_base = ($home_path === 'blog') ? 'author' : 'blog/author';
 }
 add_action('init', 'automatdo_set_author_base');
 
